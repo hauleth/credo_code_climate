cdc0bce78dc485e31e39701c526e1b18f5037a4d
Łukasz Niemier <lukasz@niemier.pl>
2021-01-17 21:28:52 +0100

    chore: cleanup mix.exs

--- mix.exs
+++ mix.exs
@@ -11,14 +11,10 @@ defmodule CredoCodeClimate.MixProject do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
-    [
-      extra_applications: [:logger]
-    ]
+    []
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:credo, "~> 1.5"},
