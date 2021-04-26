7972f4000dc282f40f83f82b1467d8834dd23817
Łukasz Niemier <lukasz@niemier.pl>
2021-04-26 10:53:51 +0200

    chore: prepare package

--- mix.exs
+++ mix.exs
@@ -4,21 +4,30 @@ defmodule CredoCodeClimate.MixProject do
   def project do
     [
       app: :credo_code_climate,
+      description: "Credo plugin for writing the CodeClimate-like result file",
       version: "0.1.0",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package()
     ]
   end
 
-  def application do
-    []
+  defp package do
+    [
+      licenses: ~w[MIT],
+      links: %{
+        "GitHub" => "https://github.com/hauleth/credo_code_climate",
+        "Credo" => "https://hex.pm/packages/credo"
+      }
+    ]
   end
 
   defp deps do
     [
       {:credo, "~> 1.5"},
-      {:jason, "~> 1.2"}
+      {:jason, "~> 1.2"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 end
